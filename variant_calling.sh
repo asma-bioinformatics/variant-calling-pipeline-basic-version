eaac086ce9d5e9247590bcb924afa86fa3e54c26
asma bouafif <bouafifasma9@gmail.com>
2026-03-18 00:45:37 +0000

    Add files via upload

---
+++ variant_calling.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Variant Calling"
+
+echo "Sample Position Reference Alternative" > ../results/variants.vcf
+echo "T1 7579472 C T" >> ../results/variants.vcf
+echo "T1 7577121 G A" >> ../results/variants.vcf
+echo "T1 7578406 C T" >> ../results/variants.vcf
+
+echo "VCF file generated"
